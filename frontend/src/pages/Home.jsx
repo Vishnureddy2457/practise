cd9ef3ac61c12ce0056210ec4a2ef234819421a7
satishmadhunala1 <118502110+satishmadhunala1@users.noreply.github.com>
2025-02-07 13:00:36 +0530

    changes in carousel

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Carousel from "./Carousel";
+const Home = () => {
+  return (
+
+      <div className=" ">
+
+      <Carousel/>
+
+      </div>
+
+
+
+  );
+};
+
+export default Home;
