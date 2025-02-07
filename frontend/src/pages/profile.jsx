791fbc3ffaa8d1d96e2372dc13aa600792237aea
Vishnureddy2457 <vishnuvishnu21801@gmail.com>
2025-02-07 11:53:01 +0530

    tailwind added

---
+++ frontend/src/pages/profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className='text-red-500 text-4xl'>This is your Profile</h1>
+    </div>
+  )
+}
+
+export default Profile;
