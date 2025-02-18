d3de08bcbe9e86c31fdc9409f4d7bb3f5b9065ad
Vishnureddy2457 <vishnuvishnu21801@gmail.com>
2025-02-18 12:46:10 +0530

    404 page adding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Profile from "./pages/profile";
 import Home from "./pages/Home";
+import Dashboard from "./components/Dashboard";
 
 // import './App.css'
 
@@ -10,11 +11,12 @@ function App() {
 
   return (
     <>
+
     <Router>
     <Navbar />
     <Routes>
       <Route path="/Home" element={<Home />} />
-      {/* <Route path="/about" element={<About />} /> */}
+      <Route path="/Dashboard" element={<Dashboard/>} />
       <Route path="/Profile" element={<Profile />} />
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
